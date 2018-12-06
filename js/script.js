6ed47d415513b14f4493a206b653e37c9d58317d
Ivan <smow32da@gmail.com>
2018-12-06 21:44:03 +0500

    Коммит

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ window.addEventListener('DOMContentLoaded', () => {
             tabContent[i].classList.remove('show');
             tabContent[i].classList.add('hide');
         }
-    }
+    };
 
     hideTabContent(1);
 
@@ -21,7 +21,7 @@ window.addEventListener('DOMContentLoaded', () => {
             tabContent[b].classList.remove('hide');
             tabContent[b].classList.add('show');
         }
-    }
+    };
 
     info.addEventListener('click', (event) => {
         let target = event.target;
@@ -41,7 +41,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     //timer
 
-    let deadline = '2018-12-05';
+    let deadline = '2018-12-16';
 
     let getTimeRemaining = (endtime) => {
         let dateDiff = (new Date()).getTimezoneOffset() * 60 * 1000;
@@ -56,7 +56,7 @@ window.addEventListener('DOMContentLoaded', () => {
             'minutes': minutes,
             'seconds': seconds
         };
-    }
+    };
 
     let timeFormat = (time) => {
         let result = time;
@@ -66,7 +66,7 @@ window.addEventListener('DOMContentLoaded', () => {
             result = '0' + time;
         }
         return result;
-    }
+    };
 
     let setClock = (id, endtime) => {
         let timer = document.getElementById(id),
@@ -88,7 +88,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
         }
 
-    }
+    };
     setClock('timer', deadline);
 
     //modal
@@ -98,10 +98,14 @@ window.addEventListener('DOMContentLoaded', () => {
         close = document.querySelector('.popup-close'),
         btnDescr = document.querySelectorAll('.description-btn');
 
-    more.addEventListener('click',() => {
+    function showModal () {
         overlay.style.display = 'block';
         this.classList.add('more-splash');
         document.body.style.overflow = "hidden";
+    }
+
+    more.addEventListener('click', function () {
+        showModal.call(this);
     });
 
     close.addEventListener('click', () => {
@@ -111,10 +115,111 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 
     for (let i = 0; i < btnDescr.length; i++) {
-        btnDescr[i].addEventListener('click', () => {
-            overlay.style.display = 'block';
-            this.classList.add('more-splash');
-            document.body.style.overflow = "hidden"
+        btnDescr[i].addEventListener('click', function () {
+            showModal.call(this);
         });
     }
+
+    //form
+
+    let massage = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с вами свяжемся!',
+        faliure: 'Что-то пошло не так...'
+    };
+
+    let form = document.querySelector('.main-form'),
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+
+    statusMessage.classList.add('status');
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        form.appendChild(statusMessage);
+
+
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        request.setRequestHeader('Content-type', 'application/json', 'charset=utf-8');
+
+        let formData = new FormData(form);
+
+        let obj = {};
+        formData.forEach(function (value, key) {
+            obj[key] = value;
+        });
+        let json = JSON.stringify(obj);
+
+        request.send(json);
+
+        request.addEventListener('readystatechange', function () {
+            if (request.readyState < 4) {
+                statusMessage.innerHTML = massage.loading;
+            } else if (request.readyState === 4 && request.status == 200) {
+                statusMessage.innerHTML = massage.success;
+            } else {
+                statusMessage.innerHTML = massage.faliure;
+            }
+        });
+        for (let i = 0; i < input.length; i++) {
+            input[i].value = '';
+        }
+    });
+
+    //contact form
+
+    let contactForm = document.getElementById('form'),
+        contactInput = contactForm.getElementsByClassName('form-Input'),
+        inputPhones = document.querySelectorAll('[type = tel]');
+
+    inputPhones.forEach(function (item) {
+        item.addEventListener('input', function () {
+            if (!checkPhone(item.value)) {
+                item.value = item.value.slice(0, -1);
+            }
+
+        });
+
+    });
+
+
+    contactForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        contactForm.appendChild(statusMessage);
+        statusMessage.style.cssText = ('color: #c78030');
+
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        request.setRequestHeader('Content-type', 'application/json', 'charset=utf-8');
+
+        let formData = new FormData(contactForm);
+
+        let obj = {};
+        formData.forEach(function (value, key) {
+            obj[key] = value;
+        });
+
+        let json = JSON.stringify(obj);
+
+        request.send(json);
+
+        request.addEventListener('readystatechange', function () {
+            if (request.readyState < 4) {
+                statusMessage.innerHTML = massage.loading;
+            } else if (request.readyState === 4 && request.status == 200) {
+                statusMessage.innerHTML = massage.success;
+            } else {
+                statusMessage.innerHTML = massage.faliure;
+            }
+        });
+        for (let i = 0; i < contactInput.length; i++) {
+            contactInput[i].value = '';
+        }
+    });
+
+    function checkPhone(text) {
+        return /^(\+|8)\d{0,10}$/.test(text);
+    }
+
 });
